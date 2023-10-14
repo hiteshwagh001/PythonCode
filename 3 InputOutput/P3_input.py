4247f123825d8ced8612e3a283c8830392eed8f9
hiteshwagh001 <hiteshswaagh001@gmail.com>
2023-10-14 17:21:48 +0530

    input output codes

---
+++ 3 InputOutput/P3_input.py
@@ -0,0 +1,11 @@
+print("movie details")
+movieName=input("enter movie name : ")
+collection=float(input('enter collection of movie : '))
+tktsold=int(input("How much tickets sold : "))
+
+
+print()
+print("movie details :")
+print("movie name : ",movieName, " collection : ",collection, " Ticket sold : ",tktsold);
+
+
---
+++ 3 InputOutput/P4_output.py
@@ -0,0 +1,53 @@
+# type 1
+x=10
+y=20;
+print()
+print(x,y)
+print()
+print(x)
+print()
+print(y)
+
+
+
+# type 2
+# print(String)
+print("\n\nhello1")
+print('hello2')
+print('''hello3''');
+print("hitesh"+"Wagh")
+print("hitesh","Wagh")
+print(3*"Hitesh ")
+print(5*"Wagh")
+
+
+
+# type 3
+# print(argument list)
+print("\n\n\n")
+x=10
+y=20
+print(x,y)
+print(x,y,sep="-")
+print(x,y,sep="*")
+
+
+# end
+print("\n\n\n")
+print(x,end=" ")
+print(y)
+
+# type 4
+x=10
+y=20.67
+str="monster"
+
+print("value of x = %d"%x)      #value of x = 10
+print("value of x = %i"%x)      #value of x = 10
+print("value of y = %f"%y)      #value of y = 20.670000
+print("value of str = %s"%str)  #value of str = monster
+
+print("value of x={} and y={}".format(x,y))     #value of x=10 and y=20.67
+
+print("value of x = %d",x) # value of x = %d 10             this is not used as a formated data it not works       
+
