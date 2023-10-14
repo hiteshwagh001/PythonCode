5385e5691676f0e60a210bf02142d376f920dba9
hiteshwagh001 <hiteshswaagh001@gmail.com>
2023-10-14 17:23:56 +0530

    add input output

---
+++ 3 InputOutput/P1_input.py
@@ -0,0 +1,25 @@
+print("give input ::")
+a=input()  #10
+print(a)
+
+b=input("enter input : ")  #20
+print(b)
+
+print(a+b) #1020
+
+testing=input("do_not_give_input_press_enter")
+print("output of the code ->>>",testing)
+print(type(testing))
+
+# demo1=None(input(),int(input()))
+# print(demo1)
+
+
+# testing= int(input("enter int : "),str(input("enter string : ")))    runtime error
+# print(testing)
+
+# testing2=input("enter input1 : ",input("enter input2 :"))   runtime error
+# print(testing2)
+
+# testing3= input("enter input : ","what  enter :")   compile time error
+# print(testing3)
---
+++ 3 InputOutput/P2_Intinput.py
@@ -0,0 +1,6 @@
+num1=int(input("enter num1 "))
+num2=int(input("enter num2 "))
+ans=num1+num2
+print(ans)
+
+
